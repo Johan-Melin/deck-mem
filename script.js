ed1f069fc265f531d538b707c146399d2876f437
Johan Melin <johan.melin@picsmart.se>
2022-08-29 12:32:32 +0200

    added shuffle and draw

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+const cardInfo = document.getElementById("card-info")
 const cardArea = document.getElementById("card-area")
 
-const url = 'https://www.deckofcardsapi.com/api/deck/new/draw/?count=2'
+const baseUrl = 'https://www.deckofcardsapi.com/api/deck/'
 
-axios.get(url).then(resp => {
-    showCard(resp.data);
-});
 
-function showCard(card){
-  let img = document.createElement("img");
-  img.src = card.cards[0].image;
-  img.alt = card.cards[0].code;
-  document.body.appendChild(img);
+let deckId = ''
+
+function shuffleDeck() {
+  const action = 'new/shuffle/?deck_count=1'
+  axios.get(baseUrl + action).then(resp => {
+    deckId = resp.data.deck_id
+  });
+  cardArea.src = ''
+  cardArea.alt = ''
+  cardInfo.innerText = "Cards remaining 52"
+}
+shuffleDeck()
+
+function nextCard() {
+  const action = deckId + '/draw/?count=1'
+  axios.get(baseUrl + action).then(resp => {
+    showCardImage(resp.data)
+  });
+}
+
+function showCardImage(card) {
+  cardInfo.innerText = "Cards remaining: " + card.remaining
+  cardArea.src = card.cards[0].image
+  cardArea.alt = card.cards[0].code
 }
